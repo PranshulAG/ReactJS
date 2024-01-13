481e5a37499213699e664c08ea135cae9a306acb
PranshulOPTI <pranshul.agarwal@optiphoenix.com>
2024-01-13 20:40:56 +0530

    starting

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("h1",{id:"heading"},"Hello From React to Pranshul Agarwal 👻");
+const parent = React.createElement("div",{id:"parent"},
+React.createElement("div",{id:"child"},
+[React.createElement("h1",{id:"h1"}," I am a H1 Tag "),React.createElement("p",{id:"p"}," Hello i am p tag ")]))
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading);
+console.log(root);
+root.render(parent);
